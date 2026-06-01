3d81227be797ee79fcae390c214f5dd69070c62c
hector <hector.lins@escola.pr.gov.br>
2026-06-01 12:16:48 -0300

    c

---
+++ script.js
@@ -0,0 +1,10 @@
+<div class="filtros-container" style="text-align: center; margin: 20px 0;">
+    <button class="btn-filtro active" data-filter="todos">Todos</button>
+    <button class="btn-filtro" data-filter="Insumo">Insumos</button>
+    <button class="btn-filtro" data-filter="Tecnologia">Tecnologia</button>
+    <button class="btn-filtro" data-filter="Sementes">Sementes</button>
+</div>
+
+<div class="card item-produto" data-category="Insumo">
+    </div>
+    
